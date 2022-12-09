c9ce64d2d71f335be2bff1dba9f41a7a3715fe25
hoaitran2606 <june26th2002@gmail.com>
2022-12-09 16:06:42 +0700

    upload chức năng login của support

--- js/sp.js
+++ js/sp.js
@@ -12,3 +12,39 @@ var acc = document.getElementsByClassName("accordion");
         }
       });
     }
+
+
+    user = localStorage.getItem("user");
+    pass = localStorage.getItem("pass");
+    var img = "/assets/";
+    function loadUser(){
+    fetch("/Data/user.json")
+    .then(function (res) {
+      if (!res.ok) {
+        throw Error("Http Error: ", res.status);
+      }
+      return res.json();
+    })
+    .then(function (data) {
+      var check = false;
+      var temp;
+      
+      for (let u of data) {
+       if ((user == u.userName) && (pass = u.passWord)) {
+          check = true;
+          img += u.avatar;
+          temp = u.userName;
+          break;
+       }
+      }
+      if (check == true){
+         document.getElementById("Login").style.display = "none";
+         document.getElementById("signUp").style.display = "none";
+         document.getElementById("account").style.display = "block";
+         document.getElementById("accImg").src = img;
+        //  document.getElementById("account__user").innerHTML = temp;
+      } else{
+      } 
+    })
+
+}
